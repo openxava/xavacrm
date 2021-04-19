a4d68b0393b8b9fb2c3d7db4e35e83661697c55d
Javier Paniza <javier@192.168.0.193>
2021-04-19 20:49:49 +0200

    Upgraded to OpenXava 6.5.1

--- XavaCRM/src/_run/_Run_XavaCRM.java
+++ XavaCRM/src/_run/_Run_XavaCRM.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class _Run_XavaCRM {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("XavaCRMDB"); // To use your own database comment this line and configure web/META-INF/context.xml
+		// DBServer.start("XavaCRMDB"); // To use your own database comment this line and configure web/META-INF/context.xml
 		AppServer.run("XavaCRM"); // Use AppServer.run("") to run in root context
 	}
 
